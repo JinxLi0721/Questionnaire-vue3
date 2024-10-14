2648af2a90ffb79d995629cbea3d2ac03c1d1075
Jinx <Jinx2021@gmail.com>
2024-10-14 17:35:21 +0800

    feat(Questionnaires): add card title & explain

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const {question} = defineProps(['question'])
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-title">
+            <h3>{{ question.title }}</h3>
+            <p>題數：{{ question.questions.length}}</p>
+        </div>
+    </div>
+</template>
--- src/views/Questionnaires.vue
+++ src/views/Questionnaires.vue
@@ -1,11 +1,44 @@
 <script setup>
+import { ref } from "vue";
+import Card from "@/components/Card.vue";
+import questionnaires from "../data/questionnaire.json";
 
+const show = ref(false);
+const qu = ref();
+
+const showModal = q => {
+    show.value = true;
+    qu.value = q;
+};
 
 </script>
+
 <template>
     <div class="container">
         <header>
             <h1>自我評估問卷</h1>
         </header>
+        <main>
+            <div class="list-container">
+                <Card
+                    v-for="question in questionnaires"
+                    :question="question"
+                    @click="showModal(question)"
+                />
+            </div>
+            <div class="modal" v-if="show">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <p class="explain-text">
+                            {{ qu.explain }}
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <a class="button" href="#">開始測驗</a>
+                        <a class="button" href="#" @click="show=false">關閉</a>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 </template>
